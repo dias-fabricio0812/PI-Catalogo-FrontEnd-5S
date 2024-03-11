ebb747c91b4b262035675374770c46a49b0c7b44
fcdias <dias.fabricio0812@gmail.com>
2024-03-10 22:58:11 -0300

    Adicionada a tela inicial do projeto

--- App.tsx
+++ App.tsx
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { View, Text, Image, Button, TouchableOpacity, StyleSheet } from 'react-native'
+import LinearGradient  from 'react-native-linear-gradient'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(){
+  return(
+    <LinearGradient 
+      colors={['#000', '#0066FF']}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 0, y: 1 }}
+      style={styles.container}>
+      <Image
+        style={styles.image}
+        source={require('./assets/reactLogo.png')}
+        resizeMode='contain'
+      />
+
+      <Text style={styles.title}>
+        Vamos
+      </Text>
+      <Text style={styles.title}>
+        Começar!
+      </Text>
+
+      <Text style={styles.text}>
+        Clique no botão abaixo para logar no app
+      </Text>
+
+      <TouchableOpacity style={styles.loginButton}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.registerButton}>
+        <Text style={[styles.text, styles.register]}>Sem cadastro? Registre-se!</Text>
+      </TouchableOpacity>
+    </LinearGradient>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'center'
   },
-});
+  image:{
+    width: 270,
+    height: 270,
+    marginTop: 40
+  },
+  title:{
+     fontSize: 55,
+     fontWeight: 'bold',
+     color: '#fff'
+  },
+  text:{
+    fontSize: 16,
+    fontWeight: '400',
+    color: '#fff'
+  },
+  loginButton:{
+    marginTop: 50,
+    backgroundColor: '#61daf8',
+    width: 235,
+    height: 60,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText:{
+    fontSize: 30,
+    fontWeight: '700',
+    color: '#fff'
+  },
+  registerButton:{
+
+  },
+  register:{
+    marginTop: 5
+  }
+})
